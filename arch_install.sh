a33292183ed0cf38113e5ac92fa1a50d3b65eff9
Felix Breuer <f.breuer94@gmail.com>
2019-03-12 20:27:45 +0100

    change  struc

---
+++ arch_install.sh
@@ -0,0 +1,9 @@
+ln -sfv ~/dotfiles/shell/_bashrc ~/.bashrc
+ln -sfv ~/dotfiles/shell/_zshrc ~/.zshrc
+ln -sfv ~/dotfiles/_tmux.conf ~/.tmux.conf
+ln -sfv ~/dotfiles/vim/_vimrc ~/.vimrc
+ln -sfv ~/dotfiles/vim/_vimrc ~/.config/nvim/init.vim
+ln -sfv ~/dotfiles/_gitconfig ~/.gitconfig
+ln -sfv ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+ln -sfv ~/dotfiles/wm/qtile.py ~/.config/qtile/config.py
--- install.sh
+++
@@ -1,7 +0,0 @@
-ln -sv ~/dotfiles/.bashrc ~/.bashrc
-ln -sv ~/dotfiles/.zshrc ~/.zshrc
-ln -sv ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sv ~/dotfiles/.vimrc ~/.vimrc
-ln -sv ~/dotfiles/.vimrc ~/.config/nvim/init.vim
-ln -sv ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sv ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
