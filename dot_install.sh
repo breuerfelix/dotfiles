f1ffc9de3773b741deb13813a342141a7572e367
Felix Breuer <f.breuer94@gmail.com>
2019-05-03 10:00:48 +0200

    dot install

---
+++ dot_install.sh
@@ -0,0 +1,6 @@
+ln -sfv ~/dotfiles/shell/.bashrc ~/.bashrc
+ln -sfv ~/dotfiles/shell/.zshrc ~/.zshrc
+ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfv ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sfv ~/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
+ln -sfv ~/dotfiles/.gitconfig ~/.gitconfig
