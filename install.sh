16c23144fbe298218783ea24fbd2246391ca5698
Felix Breuer <f.breuer94@gmail.com>
2019-09-12 16:30:50 +0200

    remove unused installer

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -sfv ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sfv ~/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
 ln -sfv ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sfv ~/dotfiles/.gitignore_global ~/.gitignore_global
+
+ln -sfv ~/dotfiles/programs/qutebrowser.py ~/.config/qutebrowser/config.py
+ln -sfv ~/dotfiles/shell/termite ~/.config/termite/config
